ebc8fbf8eb7691ae9f3e69abc0196446bf2a6311
AugustinCablant <123320681+AugustinCablant@users.noreply.github.com>
2023-12-25 19:45:44 +0100

    Sédentarité

--- functions.py
+++ functions.py
@@ -2,32 +2,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def calorie_hist(dataframe):
+def transport_pie(dataframe, column, titre):
     """ 
-    Argument : A dataframe with a column "calories"
-    Return : An histogram showing the distribution of dishes according to their calorie content 
+    Fonction qui prend en entrée un dataframe, une colonne spécifiée et le titre correspondant 
+    et qui renvoie la répartition correspondante 
     """
-    dataframe['tranche'] = (dataframe['calories'] // 50) * 50
-    counts = dataframe['tranche'].value_counts().sort_index()
-    plt.figure(figsize=(8,8))
-    plt.hist(dataframe['tranche'], bins=range(0, int(dataframe['tranche'].max()) + 50, 50), edgecolor='black', alpha=0.7)
-    plt.xlabel('Calories band')
-    plt.ylabel('Number of recipies')
-    plt.title('Breakdown of recipes by calorie band')
+    mapping_labels = {1: 'Voiture/Véhicule motorisé ', 2: 'Transport public', 3: 'A pied', 4: 'Vélo/Trottinette'}
+    dataframe[column] = dataframe[column].map(mapping_labels)
+    count_by_format = dataframe[column].value_counts()
+    plt.figure(figsize=(6, 6))
+    plt.pie(count_by_format, labels=count_by_format.index, autopct='%1.1f%%', startangle=90, colors=['#66b3ff', '#99ff99', '#ffcc99', '#ff6666'])
+    plt.title(f'{titre}')
+    plt.legend(title = "Légende", loc='upper right', bbox_to_anchor=(1.2, 0.2)) 
     plt.show()
     return None
 
-def preparation_time(dataframe):
-    """ 
-    Argument : A dataframe with a column "calories"
-    Return : An histogram showing the distribution of dishes according to their time preparation 
-    """
-    dataframe = dataframe.dropna(subset=['prep_time'])
-    dataframe['prep_time'] = dataframe['prep_time'].str.extract('(\d+)').astype(float)
-    dataframe['prep_time'] = dataframe['prep_time'].astype('Int64')
-    plt.hist(dataframe['prep_time'], bins=range(0, dataframe['prep_time'].max() + 10, 10), edgecolor='black', alpha=0.7)
-    plt.xlabel('In minutes')
-    plt.ylabel('Number of recipies')
-    plt.title('Distribution of recipes by preparation time')
+def pie_oui_non(dataframe, column, titre):
+    mapping_eps = {1: 'Oui', 2: 'Non, dispensé', 3: 'Non, pas de cours prévu', 4: 'Ne sait pas', 5: 'Refus'}
+    dataframe[column] = dataframe[column].map(mapping_eps)
+    count_by_format = dataframe[column].value_counts()
+    plt.figure(figsize=(6, 6))
+    plt.pie(count_by_format, labels=count_by_format.index, autopct='%1.1f%%', startangle=90)
+    plt.title(f'{titre}')
+    plt.legend(title = "Légende", loc='upper right', bbox_to_anchor=(1.2, 0.2)) 
     plt.show()
     return None
